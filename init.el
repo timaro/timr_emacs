411e02aed1c8086b2c0111194fd8b3635b8ff39b
Tim Robertson <timaro@gmail.com>
2011-07-01 19:28:21 -0700

    added ido mode config

--- init.el
+++ init.el
@@ -125,4 +125,31 @@
 (column-number-mode 1)
 (line-number-mode 1)
 
+;;; cleanup whitespace before save
+
 (add-hook 'before-save-hook 'whitespace-cleanup)
+
+;;; ido mode
+
+(ido-mode 'both)                   ; enable for files and buffers
+(setq ido-enable-flex-matching t)  ; for fuzzy matching of characters
+
+; Display ido results vertically, rather than horizontally
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched]" " [Not readable]" " [Too big]" " [Confirm]")))
+(defun ido-disable-line-trucation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
+
+; sort ido filelist by mtime instead of alphabetically
+(add-hook 'ido-make-file-list-hook 'ido-sort-mtime)
+(add-hook 'ido-make-dir-list-hook 'ido-sort-mtime)
+(defun ido-sort-mtime ()
+  (setq ido-temp-list
+	(sort ido-temp-list
+	      (lambda (a b)
+		(time-less-p
+		 (sixth (file-attributes (concat ido-current-directory b)))
+		 (sixth (file-attributes (concat ido-current-directory a)))))))
+  (ido-to-end  ;; move . files to end (again)
+   (delq nil (mapcar
+	      (lambda (x) (and (string-match-p "^\\.." x) x))
+	      ido-temp-list))))
